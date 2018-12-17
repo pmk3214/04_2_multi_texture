3e85a3e98026ccc423957491c89b57707beecdd9
Takashi Imagire <imagire@gmail.com>
2018-12-17 20:45:33 +0900

    Update default.js

--- default.js
+++ default.js
@@ -54,9 +54,18 @@
             'void main(void) {',
                 'float mask = texture(samplerMask, vTexCoord).x;',
                 'vec2 flow = mask * vec2(-0.10, 0.05);',
+
                 'vec4 texColor0 = texture(samplerColor, vTexCoord + weight * flow);',
                 'vec4 texColor1 = texture(samplerColor, vTexCoord + fract(weight+0.5) * flow);',
-                'outColor = mix(texColor0, texColor1, 0.5 * cos(2.0*3.141592*weight) + 0.5);',
+
+                'float w0 = texture(samplerMask, vTexCoord + weight * flow).x;',
+                'float w1 = texture(samplerMask, vTexCoord + fract(weight+0.5) * flow).x;',
+                'float w = w0 + w1;',
+            
+                'vec4 texColor0 = texture(samplerColor, vTexCoord + weight * flow);',
+                'vec4 texColor1 = texture(samplerColor, vTexCoord + fract(weight+0.5) * flow);',
+                'outColor = mix(texColor0 * w0, texColor1 * w1, 0.5 * cos(2.0*3.141592*weight) + 0.5) / w;',
+                'if(w < 0.0001) outColor = texture(samplerColor, vTexCoord);',
             '}'
         ].join('\n');
 
